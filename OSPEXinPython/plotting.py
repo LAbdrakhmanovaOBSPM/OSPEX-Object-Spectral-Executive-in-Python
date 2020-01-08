fffe9e455a4851b19750e087e6aea418b273eaa8
liaisian <liaisian.abdrakhmanova@obspm.fr>
2020-01-08 18:42:09 +0100

    Fixed merge error

--- OSPEXinPython/plotting.py
+++ OSPEXinPython/plotting.py
@@ -74,10 +74,6 @@ class Input:
         self.sum = sum(self.Time_del) # observed event duration (in seconds)
         self.detectors = str(header1[25])[10:] # load the names of used detectors 
 
-
-        # Time format conversion (from sec to h/m/sec/)
-        self.TimeNew = pd.to_datetime(self.Time, unit='s')
-
         # Time format conversion for Time - 2, 2 seconds difference
         # conversion from seconds to hours/minutes/seconds
         self.TimeNew2 = pd.to_datetime(self.Time2, unit='s')
